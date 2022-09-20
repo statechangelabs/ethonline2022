2f014bd4785b7c2bd500b4d3a7f969e93040e3c5
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-09-19 22:40:13 -0400

    feat: tested happy paths

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -14,9 +14,10 @@ async function main() {
   console.log("Seller address: ", seller.address);
   console.log("Arbiter address: ", arbiter.address);
 
+
+  // Happy Path 1: Buyer creates a job, deposits funds, seller accepts and delivers then buyer recieves and seller withdraws
   // Create a new job bid from the buyer
   console.log("Creating a new job bid from the buyer");
-  
   const jobBid = await escrow.connect(buyer).bid(ethers.utils.parseUnits("1", "ether"), seller.address, arbiter.address);
   const jobBid_receipt = await jobBid.wait();
   const jobId_1 = parseInt(jobBid_receipt.logs[0].topics[1],16)
@@ -27,15 +28,39 @@ async function main() {
   const bidAccept = await escrow.connect(seller).acceptBid(jobId_1);
   const bidAccept_receipt = await bidAccept.wait();
 
+  console.log("Asserting that the job is delivered.");
+  const jobDelivered = await escrow.connect(seller).assertDelivery(jobId_1);
+  const jobDelivered_receipt = await jobDelivered.wait();
+  console.log("Job delivered");
 
-  // const jobBid_2 = await escrow.connect(buyer).bid(ethers.utils.parseUnits("1", "ether"), seller.address, arbiter.address);
-  // const jobBid_receipt_2 = await jobBid_2.wait();
-  // // console.log(jobBid_receipt_2);
-  // console.log("Job bid created");
+  console.log("Buyer confirms the job delivery.");
+  const jobConfirmed = await escrow.connect(buyer).receiveDelivery(jobId_1);
+  const jobConfirmed_receipt = await jobConfirmed.wait();
+  console.log("Delivery confirmed");
+
+  console.log("\n-------------------\n");
+
+  // Happy Path 2: Seller creates a job, buyer accepts, seller delivers then buyer recieves and seller withdraws
+  const jobOffer = await escrow.connect(seller).offer(ethers.utils.parseUnits("1", "ether"), buyer.address, arbiter.address);
+  const jobOffer_receipt_2 = await jobOffer.wait();
+  const jobId_2 = parseInt(jobOffer_receipt_2.logs[0].topics[1],16)
+  console.log("Job Offer created");
 
   // Accept the job bid from the seller
-  // console.log("Accepting the job bid from the seller");
-  // const bidAccept = escrow.connect(seller).acceptBid()
+  console.log("Buyer Accepting the job bid from the seller");
+  const bidAccept_2 = await escrow.connect(buyer).acceptOffer(jobId_2);
+  const bidAccept_receipt_2 = await bidAccept_2.wait();
+  console.log("Offer accepted");
+
+  console.log("Seller asserts that the job is delivered.");
+  const jobDelivered_2 = await escrow.connect(seller).assertDelivery(jobId_2);
+  const jobDelivered_receipt_2 = await jobDelivered_2.wait();
+  console.log("Job delivered");
+
+  console.log("Buyer confirms the job delivery.");
+  const jobConfirmed_2 = await escrow.connect(buyer).receiveDelivery(jobId_2);
+  const jobConfirmed_receipt_2 = await jobConfirmed_2.wait();
+  console.log("Delivery confirmed");
 
 
 }
