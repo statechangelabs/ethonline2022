5bcb1150e6326d7ed83bcf401574f7f0c7146975
Ray Deck <ray@raydeck.com>
2022-09-23 11:53:38 -0400

    feat: set title to polygig

--- app/src/Title.tsx
+++ app/src/Title.tsx
@@ -5,9 +5,7 @@ const Title: FC = () => {
     <Fragment>
       <div className="flex items-center space-x-12">
         <img src={Logo} alt="Logo" className="w-28" />
-        <h1 className="text-[96px] font-bold text-primary-default">
-          HashMentor
-        </h1>
+        <h1 className="text-[96px] font-bold text-primary-default">PolyGig</h1>
       </div>
     </Fragment>
   );
